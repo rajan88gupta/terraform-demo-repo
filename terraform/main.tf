2d5d1630855b2473cba765598cd308dadd16236f
rajan88gupta <104194078+rajan88gupta@users.noreply.github.com>
2026-04-26 22:40:17 +1200

    AI Terraform update

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,7 @@
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-unique-bucket-name"
-  acl    = "private"
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name        = "MyBucket"
-    Environment = "Dev"
+    Name = "main-vpc"
   }
 }
