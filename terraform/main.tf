6cd777ab16dfb1a47a5d52b4ff0484f33384aee6
rajan88gupta <104194078+rajan88gupta@users.noreply.github.com>
2026-04-26 22:54:25 +1200

    AI Terraform update

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,8 @@
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe01a" # Replace with your desired AMI ID
+  instance_type = "t2.micro" # Replace with your desired instance type
 
   tags = {
-    Name = "my_vpc"
+    Name = "ExampleInstance"
   }
 }
